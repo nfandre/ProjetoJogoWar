ed94c1ad1ec00a6b26bcd9d787d06e18d50f0a25
nfandre <andrelive.05@hotmail.com>
2019-04-02 11:20:34 -0300

    criação dos jogadores

---
+++ src/br/edu/ifsp/spo/lg2a3/repositorio/Jogador.java
@@ -0,0 +1,6 @@
+package br.edu.ifsp.spo.lg2a3.repositorio;
+
+public class Jogador {
+	private String nome;
+	private String senha;
+}
---
+++ src/br/edu/ifsp/spo/lg2a3/repositorio/JogadorRepositorio.java
@@ -0,0 +1,5 @@
+package br.edu.ifsp.spo.lg2a3.repositorio;
+
+public class JogadorRepositorio {
+
+}
