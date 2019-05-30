e609177955f6b5abd90a526d714cb574a89ff725
nfandre <andrelive.05@hotmail.com>
2019-05-30 16:30:54 -0300

    Aplicando tests

---
+++ src/br/edu/ifsp/spo/lg2a3/repositoriosTests/ContinenteRepositorioTest.java
@@ -0,0 +1,54 @@
+package br.edu.ifsp.spo.lg2a3.repositoriosTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import br.edu.ifsp.spo.lg2a3.Jogo.Continente;
+import br.edu.ifsp.spo.lg2a3.Jogo.Dado;
+import br.edu.ifsp.spo.lg2a3.Jogo.Jogador;
+import br.edu.ifsp.spo.lg2a3.Jogo.Territorio;
+import br.edu.ifsp.spo.lg2a3.repositorio.ContinenteRepositorio;
+import br.edu.ifsp.spo.lg2a3.repositorio.JogadorRepositorio;
+
+class ContinenteRepositorioTest {
+	
+	@Test
+	void verificar_separa_territorios_jogadores() {
+		//configuração
+		Jogador j1= new Jogador("andre");
+		Jogador j2= new Jogador("alisson");
+		ArrayList<Jogador> jogadores = new ArrayList<Jogador>();
+		jogadores.add(j1);
+		jogadores.add(j2);
+		
+		Continente Africa = new Continente("Africa");	
+		Territorio AfricaSul = new Territorio("AfricaSul");
+		Territorio Congo = new Territorio("Congo");
+		Africa.Territorios.add(AfricaSul);
+		Africa.Territorios.add(Congo);
+		
+		Continente Europa = new Continente("Europa");
+		Territorio Moscou = new Territorio("Moscou");
+		Territorio Suecia = new Territorio("Suecia");
+		Europa.adicionarTerritoriosAoContinente(Moscou);
+		Europa.adicionarTerritoriosAoContinente(Suecia);
+		
+		ContinenteRepositorio.Continentes.add(Africa);
+		ContinenteRepositorio.Continentes.add(Europa);
+		ContinenteRepositorio cr = new ContinenteRepositorio();
+		
+		//execução
+		JogadorRepositorio jr = new JogadorRepositorio(jogadores);
+		cr.separarTerritoriosJogadores(jogadores.size(), jr);
+		
+		//asserção
+		for(Continente c: ContinenteRepositorio.Continentes) {
+			for(Territorio t: c.Territorios) {		
+			//	assertEquals(t.getJogadorDono(), jr.getJogadores().get(1).getTerritorios().get(index));
+			}
+		}
+	}
+}
