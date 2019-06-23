2d4fe0ba8ace5e22a4bda16c726e762ca0442e56
nfandre <andrelive.05@hotmail.com>
2019-06-23 18:05:20 -0300

    começando javafx

--- src/br/edu/ifsp/spo/lg2a3/controllerApresentacao/InicioJogarController.java
+++ src/br/edu/ifsp/spo/lg2a3/controllerApresentacao/InicioJogarController.java
@@ -1,5 +1,29 @@
 package br.edu.ifsp.spo.lg2a3.controllerApresentacao;
 
-public class InicioJogarController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import com.sun.corba.se.spi.orbutil.fsm.Action;
 
-}
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+
+
+public class InicioJogarController implements Initializable{
+
+	 @FXML private Button btnJogar;
+	
+	   
+	
+
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		btnJogar.setOnMouseClicked((MouseEvent e)->{
+			
+			App.getStage().close();
+		});
+	}}
